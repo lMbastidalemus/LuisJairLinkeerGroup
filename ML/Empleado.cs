f801518a3ec5e59dc57ef7dc60cd1ab56e911617
JGarciaRod <jgarciar271@gmail.com>
2023-10-12 17:19:54 -0600

    ML Empleado

---
+++ ML/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        
+        public string NombreNomina { get; set; }
+        
+        public string Nombre { get; set; }
+        
+        public string ApellidoPaterno { get; set; }
+        
+        public string ApellidoMaterno { get; set; }
+
+        public List<object> Empleados { get; set; }
+
+        public ML.EntidadFederativa Entidad {  get; set; }
+    }
+}
