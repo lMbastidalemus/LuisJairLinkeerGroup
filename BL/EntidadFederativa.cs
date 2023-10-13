08039495a057dcc5253f9d0b1a80ed2260d76a1b
JGarciaRod <jgarciar271@gmail.com>
2023-10-13 13:34:42 -0600

    BL GetAll entidades

---
+++ BL/EntidadFederativa.cs
@@ -0,0 +1,58 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class EntidadFederativa
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.LJLeenkedGroupEntities context = new DL.LJLeenkedGroupEntities())
+                {
+                    var query = (from CatEntidadFederativa in context.CatEntidadFederativas
+                                 select new
+                                 {
+                                     IdEstado = CatEntidadFederativa.IdEstado,
+                                     Estado = CatEntidadFederativa.Estado
+                                 }
+                    );
+                    
+                    result.Objects = new List<object>();
+
+                    if(query != null && query.ToList().Count > 0)
+                    {
+                        foreach (var item in query)
+                        {
+                            ML.EntidadFederativa entidad = new ML.EntidadFederativa();
+                            entidad.IdEstado = item.IdEstado;
+                            entidad.Estado = item.Estado;
+
+                            result.Objects.Add(entidad);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+                result.Correct = false;
+            }
+            return result;
+        }
+    }
+}
