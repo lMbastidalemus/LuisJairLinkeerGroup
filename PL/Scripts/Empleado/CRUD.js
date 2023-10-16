c007e4b509f39772bf066d44661e83bd81ad9222
JGarciaRod <jgarciar271@gmail.com>
2023-10-16 16:10:59 -0600

    modificacion GetAll crud

--- PL/Scripts/Empleado/CRUD.js
+++ PL/Scripts/Empleado/CRUD.js
@@ -9,24 +9,22 @@ function GetAll() {
         type: 'GET',
         url: 'http://localhost:58538/api/empleado/',
 
-        success: function (result) {
+        success: function (result)
+        {
             $('#tblEmpleados tbody').empty();
-            $.each(result.Objects, function (i, subcategoria) {
+            $.each(result.Objects, function (i, Empleado) {
                 var filas =
                     '<tr>'
-                    + '<td class="text-center"> '
-                    + '<a href="#" onclick="GetById(' + subCategoria.IdSubCategoria + ')">'
-                    + '</a> '
-                    + '</td>'
+                    + '<td class="text-center"> '+'<a href="#" onclick="GetById(' + Empleado.IdEmpleado + ')">' + '</a> ' + '</td>'
 
-                    + "<td  id='id' class='text-center'>" + subCategoria.IdSubCategoria + "</td>"
-                    + "<td class='text-center'>" + subCategoria.NombreNomina + "</td>"
-                    + "<td class='text-center'>" + subCategoria.Nombre + "</ td>"
-                    + "<td class='text-center'>" + subCategoria.ApellidoPaterno + "</td>"
-                    + "<td class='text-center'>" + subCategoria.ApellidoMaterno + "</td>"
-                    + "<td class='text-center'>" + subCategoria.Estado.Nombre + "</td>"
+                    + "<td  id='id' class='text-center'>" + Empleado.IdEmpleado + "</td>"
+                    + "<td class='text-center'>" + Empleado.NombreNomina + "</td>"
+                    + "<td class='text-center'>" + Empleado.Nombre + "</ td>"
+                    + "<td class='text-center'>" + Empleado.ApellidoPaterno + "</td>"
+                    + "<td class='text-center'>" + Empleado.ApellidoMaterno + "</td>"
+                    + "<td class='text-center'>" + Empleado.Estado.Nombre + "</td>"
 
-                    + '<td class="text-center"> <button class="btn btn-danger" onclick="Eliminar(' + subCategoria.IdSubCategoria + ')"><span class="glyphicon glyphicon-trash" style="color:#FFFFFF"></span></button></td>'
+                    + '<td class="text-center"> <button class="btn btn-danger" onclick="Eliminar(' + Empleado.IdEmpleado + ')"><span class="glyphicon glyphicon-trash" style="color:#FFFFFF"></span></button></td>'
 
                     + "</tr>";
                 $("#tblEmpleados tbody").append(filas);
